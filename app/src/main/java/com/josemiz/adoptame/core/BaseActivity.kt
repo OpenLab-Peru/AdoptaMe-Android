6267c3644487dd5bef1c510ddb126a40d6b8dadf
José Miguel Zea Guerrero <jzeaguerrero@gmail.com>
2019-08-20 18:05:59 -0500

    Add show error method in base activity

--- app/src/main/java/com/josemiz/adoptame/core/BaseActivity.kt
+++ app/src/main/java/com/josemiz/adoptame/core/BaseActivity.kt
@@ -3,4 +3,7 @@ package com.josemiz.adoptame.core
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
+  fun showErrorMessage(message : String){
+
+    }
 }
