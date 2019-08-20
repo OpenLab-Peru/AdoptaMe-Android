fc50dd66182b429e6e86ea2600b451e73cddb769
José Miguel Zea Guerrero <jzeaguerrero@gmail.com>
2019-08-20 18:05:13 -0500

    Add setupobserver fun in baseviewmodelactivity

--- app/src/main/java/com/josemiz/adoptame/core/BaseViewModelActivity.kt
+++ app/src/main/java/com/josemiz/adoptame/core/BaseViewModelActivity.kt
@@ -5,13 +5,12 @@ import androidx.lifecycle.ViewModel
 import com.josemiz.adoptame.core.extensions.obtainViewModel
 
 abstract class BaseViewModelActivity<V : ViewModel,T> : BaseActivity() {
-    private lateinit var viewModel : V
-    private val navigator: T? = null
+    lateinit var viewModel : V
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         viewModel = obtainViewModel(viewModel::class.java)
+        setupObservers()
     }
     
     abstract fun setupObservers()
